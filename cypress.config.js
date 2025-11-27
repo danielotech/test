fdc26ec8150d943e364f6a218cb015ae43fe7301
danielotech <daniel.teran@engineer.com>
2025-11-26 22:13:48 -0500

    Test PR

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('cypress');
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://www.saucedemo.com',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/e2e.js',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+  },
+});
